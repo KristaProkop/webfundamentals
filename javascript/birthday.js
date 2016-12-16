16312cf6b3301a9c37e2f25c82a5128c2e85dad2
kristaprokop <krista.prokop@gmail.com>
2016-12-16 17:48:01 -0600

    For loop birthday countdown

---
+++ javascript/birthday.js
@@ -0,0 +1,17 @@
+
+// birthday countdown using for loop
+
+daysUntilMyBirthday = 40
+
+for (daysUntilMyBirthday; daysUntilMyBirthday > 0; daysUntilMyBirthday = daysUntilMyBirthday - 1)
+ {
+  if (daysUntilMyBirthday < 5)
+    console.log(daysUntilMyBirthday + " DAYS UNTIL MY BIRTHDAY");
+  if (daysUntilMyBirthday > 30) 
+    console.log(daysUntilMyBirthday + " days until my birthday. Such a long time...");
+   if  (daysUntilMyBirthday <= 30 && daysUntilMyBirthday > 5)
+  console.log(daysUntilMyBirthday + " days until my birthday... it's coming!");
+
+}
+ console.log("TODAY IS MY BIRTHDAY!!");  
+
