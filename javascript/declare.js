311a539f1459bd9b753e8adf58a904a6c3191ce7
kristaprokop <krista.prokop@gmail.com>
2016-12-15 18:58:58 -0600

    JS Declarations

---
+++ javascript/declare.js
@@ -0,0 +1,20 @@
+var number1 = 22
+var number2 = -27
+var number3 = 999
+var number4 = 0.1
+
+var string1 = "Puppy"
+var string2 = "Second helpings"
+var string3 = "Merry freakin' Christmas"
+
+console.log(4 == 2 + 3)
+console.log(2 != false)
+
+console.log("THe first number is " + number1)
+console.log(number2 + " is less than zero" )
+console.log(number3 + " is the third number")
+console.log("our next number, " + number4 + ", is the final number")
+
+console.log("I want a " + string1)
+console.log(string2 + " please")
+console.log("Have yourself a " + string3)
